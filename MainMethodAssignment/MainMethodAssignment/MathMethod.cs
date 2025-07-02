a935f38880e0f78334d7904cd6f8b588873435c7
amanamard <104510239+amanamard@users.noreply.github.com>
2025-07-01 23:20:51 -0400

    Added my main method assignment.

---
+++ MainMethodAssignment/MainMethodAssignment/MathMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class MathMethod
+    {
+        //A method that takes in an integer, add 7 to it and return the answer as an integer
+        public int MathOperation(int a)
+        {
+            return a + 7;
+        }
+
+       //A method that takes in a decimal, multiply it by 7 and return the answer as an integer
+        public int MathOperation(decimal a) 
+        {
+            return (int)a * 9;
+        }
+
+        //A method that takes in a string, convert it to an integer and return the remainder as an integer
+        public int MathOperation(string a) 
+        {
+            int num = Convert.ToInt32(a);
+            return num % 2;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating the class MathMethod
+            MathMethod mathMethod = new MathMethod();
+            //Calling the first method with an integer argument and assigning the result to the variable x
+            int x = mathMethod.MathOperation(5);
+            //Displaying x on the screen
+            Console.WriteLine(x);
+            
+            //Calling the second method with a decimal argument and assigning the result to the variable y
+            int y = mathMethod.MathOperation(5.03230923m);
+            //Displaying x on the screen
+            Console.WriteLine(y);
+           
+            //Calling the third method with a string argument and assigning the result to the variable z
+            int z = mathMethod.MathOperation("15");
+            Console.WriteLine(z);
+            Console.ReadLine();
+
+        }
+    }
+}
