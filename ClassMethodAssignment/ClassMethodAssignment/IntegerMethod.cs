994286cdc397fbeeed486a9f2ad7854b073ae9f2
amanamard <104510239+amanamard@users.noreply.github.com>
2025-07-03 13:22:18 -0400

    Added my class method assignment.

---
+++ ClassMethodAssignment/ClassMethodAssignment/IntegerMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassMethodAssignment
+{
+    //Creating IntegerMethod class
+    class IntegerMethod
+    {
+        //A void method that outputs an integer
+        public void IntegerOperation(int a)
+        {
+            int b = a / 2;
+            Console.WriteLine(a + " / 2 = " + b);
+        }
+
+        //An overload method with output parameters
+        public int IntegerOperation(int a, out int b)
+        {
+            b = 0;
+            int c = a * 2;
+            b++;
+            return c;
+        }
+    }
+}
