58e0642ede5569133dfd181d4a2221e2db835772
amanamard <104510239+amanamard@users.noreply.github.com>
2025-07-02 20:58:44 -0400

    Added my method assignment.

---
+++ MethodAssignment/MethodAssignment/MathMethod2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class MathMethod2
+    {
+        //Creating a method that takes two integers as parameters
+        //Making one of them optional by providing a default value
+        public int MathOperation2(int a, int b = 0)
+        {
+            //Performing addition on integers and returning the result
+            return a + b;
+        }
+    }
+}
