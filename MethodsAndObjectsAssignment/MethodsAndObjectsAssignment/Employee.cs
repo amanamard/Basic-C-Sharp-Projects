b96fc5d5e802d3451dc661dceb6bab4cc442dacf
amanamard <104510239+amanamard@users.noreply.github.com>
2025-07-03 21:23:01 -0400

    Added my methods and objects assignment.

---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MethodsAndObjectsAssignment
+{
+    //Creating class Employee that inherit from class Person
+    public class Employee : Person
+    {
+        //Property
+        public int Id { get; set; }
+    }
+}
---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MethodsAndObjectsAssignment
+{
+    //Creating class Person
+    public class Person
+    {
+        //Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //A void method
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
