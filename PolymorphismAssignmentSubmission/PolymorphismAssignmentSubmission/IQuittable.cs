a0de8dd0a9618009524901d1065adf13af568391
amanamard <104510239+amanamard@users.noreply.github.com>
2025-07-05 00:43:47 -0400

    Added my polymorphism assignment submission.

---
+++ PolymorphismAssignmentSubmission/PolymorphismAssignmentSubmission/IQuittable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PolymorphismAssignmentSubmission
+{
+    //An interface called IQuittable
+    interface IQuittable
+    {
+        //A void method called Quit()
+        void Quit(Employee employee);
+    }
+}
