9ea0324e18377cc0015e379bd0682162507ecbe8
amanamard <104510239+amanamard@users.noreply.github.com>
2025-07-04 22:38:34 -0400

    Added my abstract class assignment.

---
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbstractClassAssignment
+{
+    //Creating class Employee that inherited from class Person
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        //Implementing the SayName() method
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
