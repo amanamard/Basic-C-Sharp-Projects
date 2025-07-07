366019f7a6b580262c1c5bf45157885a6ab07287
amanamard <104510239+amanamard@users.noreply.github.com>
2025-07-06 23:54:53 -0400

    Added my operators assignment submission.

---
+++ OperatorsAssignmentSubmission/OperatorsAssignmentSubmission/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatorsAssignmentSubmission
+{
+    //Employee class
+    public class Employee
+    {
+        //Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Overload the "==" operator
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+
+        //Overload the "!=" operator
+        public static bool operator!= (Employee employee1, Employee employee2)
+        { 
+            return !(employee1.Id == employee2.Id);
+        }
+
+    }
+}
