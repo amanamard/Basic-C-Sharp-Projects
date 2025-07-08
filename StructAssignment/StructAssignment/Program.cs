717cb784718a9aa65b9c7edc27ab88da0e2b60cc
amanamard <104510239+amanamard@users.noreply.github.com>
2025-07-08 18:43:02 -0400

    Added my struct assignment.

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //An object of data type Number
+            Number number = new Number();
+            //Assigning an amount to the object
+            number.Amount = 100000m;
+            //Printing the amount
+            Console.WriteLine("My annual salary is: " + "$" + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
