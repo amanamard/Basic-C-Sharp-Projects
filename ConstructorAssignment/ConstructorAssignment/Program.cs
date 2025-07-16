bac1cf9bb78854c6efd8831928fdca47ffc10481
amanamard <104510239+amanamard@users.noreply.github.com>
2025-07-15 20:41:31 -0400

    Added my constructor assignment.

---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a const variable
+            const string assignmentName = "Constructor Assignment";
+            Console.WriteLine("Welcome to the {0}.\n", assignmentName);
+            //Creating a variable using the keyword "var"
+            var newUser = new User("Ben");
+            Console.WriteLine("{0} is {1} years old.", newUser.userName, newUser.userAge);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    public class User
+    {
+        //Chaining of two constructors
+        public User(string name) : this(name, 46) 
+        {
+        }
+        public User(string name, int age) 
+        {
+            this.userName = name;
+            this.userAge = age;
+        }
+        //Properties
+        public int userAge { get; set; }
+        public string userName { get; set; }
+    }
+}
